c417ba556fc10d9b0e8a3ab8fe97c9f3ab759307
Eric Masi <eric.masi.dev@gmail.com>
2025-08-28 16:42:48 -0400

    Fix bug

--- migrations/005_add_earnings_table.sql
+++ migrations/005_add_earnings_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS earnings(
     id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    amount numeric(5, 4) NOT NULL,
+    amount numeric(9, 4) NOT NULL,
     date_of_work date NOT NULL,
     payroll_id bigint REFERENCES payrolls(id) NOT NULL,
     worker_id bigint REFERENCES workers(id) NOT NULL,
     crew_id bigint REFERENCES crews(id),
-    hours_worked numeric(2, 4),
-    hours_offered numeric(2, 4),
-    piece_units numeric(8, 4)
+    hours_worked numeric(6, 4),
+    hours_offered numeric(6, 4),
+    piece_units numeric(9, 4)
 );
 
 ---- create above / drop below ----
